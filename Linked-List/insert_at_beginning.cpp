fd91a1285576c24ba138747bb7283790d7da9af8
snacksncode <snacksncode@gmail.com>
2021-08-08 23:32:46 +0200

    [ Linked List ] Linked list with insertion at the beginning

---
+++ Linked-List/insert_at_beginning.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+// defining Node struct
+struct Node
+{
+  int data;
+  Node *next;
+};
+
+using namespace std;
+
+Node *InsertIntoListAtBeginning(Node *head, int data)
+{
+  Node *temp = new Node();
+  temp->data = data;
+  temp->next = head;
+  head = temp;
+  return head;
+}
+
+void PrintList(Node *head)
+{
+  printf("List is: [");
+  while (head != NULL)
+  {
+    printf(" %d", head->data);
+    head = head->next;
+  }
+  printf(" ]\n");
+}
+
+int main()
+{
+  Node *head = NULL;
+  printf("How many numbers will be in linked list?\n");
+  int amountOfNumbers, enteredNumber;
+  scanf("%d", &amountOfNumbers);
+  for (int i = 0; i < amountOfNumbers; i++)
+  {
+    printf("Enter the number...\n");
+    scanf("%d", &enteredNumber);
+    head = InsertIntoListAtBeginning(head, enteredNumber);
+    PrintList(head);
+  }
+  return 0;
+}
